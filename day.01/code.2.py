34a260b53ad08fb0284673627d6915531c53a18f
Posen Chiu <posen2101024@gmail.com>
2022-12-11 17:47:19 +0800

    Solve day 01

---
+++ day.01/code.2.py
@@ -0,0 +1,18 @@
+
+def main(data):
+    ans = [0]
+
+    for num in data.strip().split('\n'):
+        if num == '':
+            ans.append(0)
+            continue
+        ans[-1] += int(num)
+
+    return sum(sorted(ans)[-3:])
+
+
+if __name__ == '__main__':
+    with open('sample.in', 'rb') as fp:
+        print(f'Sample:\n{main(fp.read().decode())}')
+    with open('puzzle.in', 'rb') as fp:
+        print(f'Puzzle:\n{main(fp.read().decode())}')
