787570f1b856fa69c982e040bc98a5797e9dcf30
Posen Chiu <posen2101024@gmail.com>
2022-12-14 12:09:08 +0800

    Solve day 04

---
+++ day.04/code.2.py
@@ -0,0 +1,18 @@
+
+def main(data):
+    ans = 0
+    for s in data.strip().split():
+        l, r = s.split(',')
+        lx, ly = map(int, l.split('-'))
+        rx, ry = map(int, r.split('-'))
+        if ly < rx or ry < lx:
+            continue
+        ans += 1
+    return ans
+
+
+if __name__ == '__main__':
+    with open('sample.in', 'rb') as fp:
+        print(f'Sample:\n{main(fp.read().decode())}')
+    with open('puzzle.in', 'rb') as fp:
+        print(f'Puzzle:\n{main(fp.read().decode())}')
